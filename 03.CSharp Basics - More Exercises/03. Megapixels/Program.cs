5976d4b7d96927254a60b5a89e7f71592336d377
21wildcrybabies <tomovich47@gmail.com>
2017-06-01 00:17:11 +0300

    CSharp Basics - More Exercises

---
+++ 03.CSharp Basics - More Exercises/03. Megapixels/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Megapixels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long width = long.Parse(Console.ReadLine());
+            long height = long.Parse(Console.ReadLine());
+            double megapixels = Math.Round(width * height / 1000000.0, 1);
+            Console.WriteLine($"{width}x{height} => {megapixels}MP");
+        }
+    }
+}
---
+++ 03.CSharp Basics - More Exercises/04. Photo Gallery/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Photo_Gallery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int photosNumber = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            long size = long.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: DSC_{photosNumber:D4}.jpg");
+            Console.WriteLine($"Date Taken: {day:D2}/{month:D2}/{year:D2} {hour:D2}:{minutes:D2}");
+            if (size < 1000)
+            {
+                Console.WriteLine($"Size: {size}B");
+            }
+            else if (size < 1000000)
+            {
+                size /= 1000;
+                Console.WriteLine($"Size: {size}KB");
+            }
+            else
+            {
+                size /= 1000000;
+                Console.WriteLine($"Size: {size}MB");
+            }
+            if (width > height)
+            {
+                Console.WriteLine($"Resolution: {width}x{height} (landscape)");
+            }
+            else if (height > width)
+            {
+                Console.WriteLine($"Resolution: {width}x{height} (portrait)");
+            }
+            else if (height == width)
+            {
+                Console.WriteLine($"Resolution: {width}x{height} (square)");
+            }
+        }
+    }
+}
